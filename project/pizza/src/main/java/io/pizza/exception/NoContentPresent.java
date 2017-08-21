e937875e1a0b4059bfa803e09846ae1f7bb1ad6f
kaushik nandhan <kaushiknandhan@yahoo.com>
2017-08-21 13:25:10 -0700

    feat(module-master): modify exception handling by adding response-status

--- project/pizza/src/main/java/io/pizza/exception/NoContentPresent.java
+++ project/pizza/src/main/java/io/pizza/exception/NoContentPresent.java
@@ -1,5 +1,9 @@
 package io.pizza.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code=HttpStatus.BAD_REQUEST)
 public class NoContentPresent extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
